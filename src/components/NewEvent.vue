97f92521acfc8b618e32155b570c880016f85179
Mike de Groot <mike.j.degroot@gmail.com>
2017-09-27 11:15:42 +0100

    front end router set up but no pages rendering

---
+++ src/components/NewEvent.vue
@@ -0,0 +1,1 @@
+<h1>New Event Page</h1>
---
+++ src/index.js
@@ -0,0 +1,37 @@
+
+import Vue from 'vue';
+import App from './components/App.vue';
+import Home from './components/Home.vue';
+import NewEvent from './components/NewEvent.vue';
+import Signup from './components/Signup.vue';
+import Login from './components/Login.vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+export var router = new VueRouter();
+
+// Set up routing and match routes to components
+router.map({
+  '/home': {
+    component: Home
+  },
+  '/newevent': {
+    component: NewEvent
+  },
+  '/login': {
+    component: Login
+  },
+  '/signup': {
+    component: Signup
+  }
+});
+
+// Redirect to the home route if any routes are unmatched
+router.redirect({
+  '*': '/home'
+});
+
+// Start the app on the #app div
+router.start(App, '#app');
